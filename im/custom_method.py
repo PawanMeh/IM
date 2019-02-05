988fb9310c396002e09e0a90e05cfb3bc501bd11
pawan <pawan@erpnext.com>
2019-02-05 20:21:55 +0530

    changes

--- im/custom_method.py
+++ im/custom_method.py
@@ -10,7 +10,7 @@ def make_multiple_issues(docname):
 	cur_issue = frappe.get_doc("Issue", docname)
 	issue_inserted = False
 	if cur_issue.status in ["Open", "Replied"]:
-		con_name = "Multiple issues raised: "
+		con_name = " "
 		for row in cur_issue.get("issue_split"):
 			issue = frappe.new_doc("Issue")
 			issue.subject = row.subject
@@ -19,7 +19,7 @@ def make_multiple_issues(docname):
 			issue.issue_type = cur_issue.issue_type
 			issue.issue_reference = cur_issue.name
 			issue.insert(ignore_permissions=True)
-			con_name = con_name + issue.name + " / "
+			con_name = row.subject + "-" + issue.name + " / "
 			issue_inserted = True
 		if issue_inserted:
 			cur_issue.resolution_details = con_name
@@ -32,8 +32,8 @@ def make_multiple_issues(docname):
 
 	if cur_issue.raised_by and issue_inserted:
 		sender_email = frappe.db.get_value("Email Account",cur_issue.email_account, "email_id")
-		msg = """ Hi, <br><br> Issue - %s has been split and multiple issues have been raised as per resolution: %s"""%(cur_issue.name, cur_issue.resolution_details)
-		frappe.sendmail(sender = sender_email, recipients = [cur_issue.raised_by], subject = "Issue Split -'%s'"%(cur_issue.name), content = msg)
+		msg = """ Hi, <br><br> Ticket No - %s has been split and replaced with the following new ticket numbers <br><br> %s"""%(cur_issue.name, con_name)
+		frappe.sendmail(sender = sender_email, recipients = [cur_issue.raised_by], subject = "Ticket No -'%s' is split and replaced"%(cur_issue.name), content = msg)
 
 @frappe.whitelist()
 def cc_list(docname):
@@ -95,6 +95,7 @@ def share_issue(self, method):
 			pass
 		else:
 			sender_email = frappe.db.get_value("Email Account",self.email_account, "email_id")
-			msg = """ Hi, <br><br> Issue - %s has been closed"""%(self.name)
-			frappe.sendmail(sender = sender_email, recipients = [self.raised_by], subject = "Issue -'%s' has been closed. <br> Please do not reply to this email. In case you are not satisfied with the resolution, please raise a new issue"%(self.name), content = msg)
+			msg = """ Hi, <br><br> %s - Ticket No - %s has been closed. <br><br>In case you do not agree with the closure kindly send a fresh email with reasons for your disagreement.
+					<br><br>Kindly mention the following as your subject line. <br><br><b> %s - Ticket No - %s Closure is Dsiagreed <b>"""%(self.name, self.subject, self.name, self.subject)
+			frappe.sendmail(sender = sender_email, recipients = [self.raised_by], subject = "Ticket No -'%s' is Closed"%(self.name), content = msg)
 		
